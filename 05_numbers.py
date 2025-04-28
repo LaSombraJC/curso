f1d5e0709f4d090ca7390e7690137964b3aaf576
LaSombraJC <162064487+LaSombraJC@users.noreply.github.com>
2025-04-28 10:56:10 -0600

    variables  tipo  numerica python
    declaracion de variables

---
+++ 05_numbers.py
@@ -0,0 +1,45 @@
+
+#Clase para aprender tipo de datos nativo de numeros
+lives = 3
+#Esta sentencia imprime el tipo de dato de las variables
+print(type(lives))
+age =18
+buget = 100
+precio = 12.2
+print(type(precio))
+
+
+lives = 2
+
+print(lives)
+
+lives = 12+8
+print(lives)
+
+lives = lives-1
+print(lives)
+
+#Una forma diferente de decrementar una variable
+lives -= 1
+print(lives)
+#Una forma diferente de incrementar una variable
+lives +=1
+print(lives)
+
+number = 0.0000000000000000001
+print(number)
+#EJERCICIO DEL VIDEO; CREAR UN PROGRAMA QUE CALCULE EL PROMEDIO DE UNA EMPRESA 
+
+print("CALCULO DEL PRESUPUESTO PARA TRES MESES")
+print("------------------------------------")
+Mes_1 =int(input("Ingrese el presupuesto del primer mes: "))
+print(type(Mes_1))
+Mes_2 = int(input("Ingrese el presupuesto del segundo mes: "))
+Mes_3 = int(input("Ingrese el presupuesto del tercer mes: "))
+#Calculo del promedio del mes
+Promedio =float( Mes_1+ Mes_2+Mes_3)/3
+#Imprimimos los resultados
+print("EL RESULTADO DEL PROMEDIO ES EL SIGUIENTE:    ")
+print("$ ",Promedio)
+print("¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡GRACIAS POR USAR LA APLICACION!!!!!!!!!!!!!!!!")
+
